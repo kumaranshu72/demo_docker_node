b1c2836f982af3107b0be3abc0478ef9b084b6e9
anshu kumar <anshu.kumar@ebizontek.com>
2019-01-25 13:32:31 +0530

    swagger included

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 import express from 'express'
 
 var app = express()
+var router = express.Router()
 
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 
-//Define request response in root URL (/)
-app.get('/', (req, res) => {
-  var {a, b, c} = {a: 1, b: 2, c: 3};
-  console.log(a);
-  res.send('Hello World!')
-})
+app.use('/', router)
+
+router.get('/', function(req, res){
+   console.log('hi');
+   res.send('hello world!');
+});
 
 //Launch listening server on port 8081
 app.listen(3000, function () {
